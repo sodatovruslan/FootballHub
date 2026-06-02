a36980ba13c172becffdef30db435bb36117824f
sodatovruslan <email@github.com>
2026-06-02 15:46:56 +0300

    add team in football model

--- football/models.py
+++ football/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    logo = models.ImageField(upload_to='teams/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
 # Create your models here.
