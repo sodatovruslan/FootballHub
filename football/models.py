f4909e4eae550179a21983dafab3159279e1ce72
sodatovruslan <email@github.com>
2026-06-02 15:51:26 +0300

    add Tournament in football model

--- football/models.py
+++ football/models.py
@@ -29,4 +29,10 @@ class Player(models.Model):
 
     def __str__(self):
         return self.full_name
+class Tournament(models.Model):
+    name = models.CharField(max_length=100)
+    season = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
