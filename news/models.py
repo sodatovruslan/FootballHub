66af2c64f10ae99146d6fda6c43739d38ce42068
sodatovruslan <email@github.com>
2026-06-04 07:31:19 +0300

    fix the models

--- news/models.py
+++ news/models.py
@@ -19,9 +19,14 @@ class Comment(models.Model):
     news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='comments')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
+    parent_comment = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.news.title}"
+
+    @property
+    def is_reply(self):
+        return self.parent_comment is not None
 
 # Create your models here.
